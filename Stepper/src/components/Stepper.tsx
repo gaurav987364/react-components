b2309b6bb7ced7aee4b26eaf5f57c23bc30bab7c
gaurav <145903987+gaurav987364@users.noreply.github.com>
2024-11-27 12:40:43 +0530

    ....Stepper

--- Stepper/src/components/Stepper.tsx
+++ Stepper/src/components/Stepper.tsx
@@ -1,38 +1,96 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import {useEffect, useRef, useState} from "react";
 
-import { useState } from "react"
-
-
-interface Props{
-    configs: object[]
+interface Props {
+  configs: {
+    Component: React.FC<any>;
+    name: string;
+  }[];
 }
+const CheckoutStepper = ({configs} : Props) => {
+  const [currentStep, setCurrentStep] = useState(1);
+  const [isComplete, setIsComplete] = useState(false);
+  const [margins, setMargins] = useState({
+    marginLeft: 0,
+    marginRight: 0,
+  });
+  const stepRef = useRef<HTMLDivElement[]>([]);
 
-const Stepper = ({configs}:Props) => {
-    const [currentStep, setCurrentStep] =  useState<number>(1);
-    const [isComplete, setIsComplete] = useState(false);
+  useEffect(() => {
+    setMargins({
+      marginLeft: stepRef.current[0].offsetWidth / 2,
+      marginRight: stepRef.current[configs.length - 1].offsetWidth / 2,
+    });
+  }, [stepRef, configs.length]);
 
+  const handleNext = () => {
+    setCurrentStep((prevStep) => {
+      if (prevStep === configs.length) {
+        setIsComplete(true);
+        return prevStep;
+      } else {
+        return prevStep + 1;
+      }
+    });
+  };
 
-    if(!configs.length){
-        return <></>;
-    }
+  const calculateProgressBarWidth = () => {
+    return ((currentStep - 1) / (configs.length - 1)) * 100;
+  };
+
+  const ActiveComponent = configs[currentStep - 1]?.Component;
+  if (!configs.length) {
+    return <></>;
+  }
   return (
     <>
-    <div className="stepper">
-       {configs.map((step:any,idx)=>(
-         <div key={step?.name} className={`step ${currentStep>idx+1 || isComplete? "complete":""} ${currentStep === idx+1 ? "active" : ""}`}>
-            <span className="step-number">{idx+1}</span>
-            <span className=" step-name">{step.name}</span>
-         </div>
-       ))}
-    </div>
+      <div className="stepper">
+        {configs.map((step:any, index:number) => {
+          return (
+            <div
+              key={step.name}
+              // @ts-expect-error
+              ref={(el) => (stepRef.current[index] = el)}
+              className={`step ${
+                currentStep > index + 1 || isComplete ? "complete" : ""
+              } ${currentStep === index + 1 ? "active" : ""} `}
+            >
+              <div className="step-number">
+                {currentStep > index + 1 || isComplete ? (
+                  <span>&#10003;</span>
+                ) : (
+                  index + 1
+                )}
+              </div>
+              <div className="step-name">{step.name}</div>
+            </div>
+          );
+        })}
 
-    {!isComplete && (
-        <button className="btn" onClick={()=>{}}>
-        {currentStep === configs.length ? "Finish" : "Next"}
+        <div
+          className="progress-bar"
+          style={{
+            width: `calc(100%-${margins.marginLeft + margins.marginRight}px)`,
+            marginLeft: margins.marginLeft,
+            marginRight: margins.marginRight,
+          }}
+        >
+          <div
+            className="progress"
+            style={{width: `${calculateProgressBarWidth()}%`}}
+          ></div>
+        </div>
+      </div>
+
+      <ActiveComponent />
+
+      {!isComplete && (
+        <button className="btn" onClick={handleNext}>
+          {currentStep === configs.length ? "Finish" : "Next"}
         </button>
-    )}
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Stepper
+export default CheckoutStepper;
