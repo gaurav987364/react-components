945fa2b8d4ea99ef2d9df270f61ff5ea1c8ed835
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-01-07 15:20:54 +0530

    ..

--- image-caraousal/src/App.tsx
+++ image-caraousal/src/App.tsx
@@ -1,6 +1,31 @@
+import IMGCAR from "./components/IMGCAR"
+
+const IMG_Data = [
+  {
+    title: "Title 1",
+    image: "https://images.pexels.com/photos/30037821/pexels-photo-30037821/free-photo-of-peaceful-walkway-in-verdant-garden-with-spirals.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    description: "Description 1",
+  },
+  {
+    title: "Title 2",
+    image: "https://images.pexels.com/photos/30097303/pexels-photo-30097303/free-photo-of-neuschwanstein-castle-in-lush-bavarian-forest.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    description: "Description 2",
+  },
+  {
+    title: "Title 3",
+    image: "https://images.pexels.com/photos/29874794/pexels-photo-29874794/free-photo-of-magpie-perched-on-a-birch-branch-in-autumn-forest.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    description: "Description 3",
+  },
+]
 const App = () => {
   return (
-    <div>App</div>
+    <div className=" w-full h-screen p-5 flex justify-center bg-gray-800 text-neutral-50">
+      <IMGCAR>
+        {IMG_Data.map((img)=>(
+          <img className=" w-full h-full object-cover" src={img.image} alt={img.title}/>
+        ))}
+      </IMGCAR>
+    </div>
   )
 }
 
---
+++ image-caraousal/src/components/IMGCAR.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react"
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
+interface Props extends React.PropsWithChildren{
+    title?: string;
+    image?: string;
+    description?: string;
+}
+
+const IMGCAR : React.FC<Props> = ({children:slides}) => {
+    const [curr,setCurr] = useState<number>(0);
+
+    const handelPrev = ()=>{
+        setCurr((c)=>(c === 0 ? slides.length -1 : curr-1))
+    };
+    const handelNext = ()=>{
+        setCurr((c)=>(c === slides.length -1? 0 : curr+1))
+    };
+  return (
+    <div className=" w-[30vw] h-[30vw] border relative overflow-hidden">
+        <h2 className=" flex w-full transition-transform ease-out duration-500" style={{transform:`translateX(-${curr*100}%)`}}>{slides}</h2>
+
+        <div className=" absolute inset-0 flex items-center justify-between">
+            <button onClick={handelPrev}><BiChevronLeft fill="black" size={40} className=" p-2 bg-gray-100/60 rounded-full"/></button>
+            <button onClick={handelNext}><BiChevronRight fill="black" size={40} className=" p-2 bg-gray-100/60 rounded-full"/></button>
+        </div>
+
+        <div>
+        </div>
+    </div>
+  )
+}
+
+export default IMGCAR
