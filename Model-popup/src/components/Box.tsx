6e96853f04c10cb95af1b9cfa0cf7d59dad73e88
gaurav987364 <sharmagourav9654@gmail.com>
2025-06-01 18:37:41 +0530

    ;;;

--- Model-popup/src/components/Box.tsx
+++ Model-popup/src/components/Box.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 const Box = ({content} : Props) => {
   return (
-    <div className=" flex justify-center items-center h-[30rem] w-[50rem] rounded-md bg-zinc-300 max-sm:w-[25rem] max-sm:h-[18rem]">
+    <div className="flex justify-center items-center h-[30rem] w-[50rem] rounded-md bg-zinc-300 max-sm:w-[25rem] max-sm:h-[18rem]">
         {content}
     </div>
   )
