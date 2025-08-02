7ddabf64cf9dc4d9e8342ab696490b3052c217f6
gaurav987364 <sharmagourav9654@gmail.com>
2025-08-02 15:25:03 +0530

    fix timing of toast

--- wordle_game/src/App.tsx
+++ wordle_game/src/App.tsx
@@ -18,7 +18,7 @@ const App = () => {
           reverseOrder={false}
           position="top-center"
           toastOptions={{
-            duration: 2000,
+            duration: 500,
           }}
         />
     </ThemeProvider>
